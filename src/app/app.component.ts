8ce576b3a22195ad31db61a786534ec4d69ba3d3
Denny <calgary.dany@gmail.com>
2022-05-14 20:22:56 +0300

    2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-frontend';
+  port: number = 3000;
+  res: any = 'placeholder';
+  // URL: string = `http://localhost:${this.port}/`
+  URL: string = 'https://api.punkapi.com/v2/beers?page=1&per_page=1'
+
+  ngOnInit () {
+    this.dataDefinitionFromRequest()
+  }
+
+  serverRequest(URL: string): any {
+    fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.res = data[0]
+    });
+  }
+
+  dataDefinitionFromRequest() {
+    try {
+      this.res = this.serverRequest(this.URL);
+    } catch(e) {
+      console.log(e)
+    }
+    console.log(this.res)
+  }
 }
