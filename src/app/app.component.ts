e9540c0b884ff5d7ab585078e09c770ff1f90231
Denny <calgary.dany@gmail.com>
2022-05-24 14:54:16 +0300

    add request to server, toggle, delete, add tasks functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,65 @@ import { task, taskObj } from './types/types';
 })
 export class AppComponent {
   title = 'angular-todo-frontend';
-  port: number = 3000;
-  res: any = 'placeholder';
+  data: any = 'placeholder';
   newTodotask: string = '';
-  // URL: string = `http://localhost:${this.port}/`
+  URL: string = `http://localhost:3000/get-to-front`
+  URLtoPost: string = `http://localhost:3000/post-from-front`
+  URLtoDelete: string = `http://localhost:3000/delete-from-front`
+  URLtoPut: string = `http://localhost:3000/put-from-front`
   tempArrayOfTodos: task[] = [
-    {id:1, title: "покушать", status: true},
-    {id:2, title: "покакать", status: true},
-    {id:3, title: "почесаться", status: false},
-    {id:4, title: "набрать воды", status: false},
-    {id:5, title: "пригтовить ужин", status: true},
-    ]; // временно, позже получаем из локалхост/3000 как this.res
+    // {id:7, title: "покушать", status: true}
+  ];
 
   ngOnInit (): void {
-    // this.serverRequest(this.URL)
+    this.serverRequestGet(this.URL)
   }
 
-  serverRequest(URL: string): void {
+  serverRequestGet(URL: string): void {
     fetch(URL)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      this.res = data
+      this.tempArrayOfTodos = data
+      console.log(`гет отработал и передал нам ${data}`)
     });
   }
 
-  toggleStyleCheck(todo: task) {
+  async serverRequestPost(URL: string, data: task): Promise<any> {
+    const response = await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return await response.json();
+  }
+
+  async serverRequestPut(URL: string, data: task): Promise<any> {
+    const response = await fetch(URL, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return await response.json();
+  }
+  
+  async serverRequestDelete(URL: string, data: task): Promise<any> {
+    const response = await fetch(URL, {
+      method: 'DELETE',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return await response.json();
+  }
+
+  toggleStyleCheck(todo: task): Object {
     if(todo?.status) {
       return {'color': 'green'}
     } else {
@@ -42,22 +74,40 @@ export class AppComponent {
     }
   }
 
+  foundFreeIndex(tempArrayOfTodos: task[]): number {
+    let arrayOfIndexes: any[] = []
+    for (let i=0; i<tempArrayOfTodos.length; i++) {
+      arrayOfIndexes.push(tempArrayOfTodos[i]?.id)
+    }
+    for (let j=0; j<(tempArrayOfTodos.length+5); j++) {
+      if (!arrayOfIndexes.includes(j)) {
+        return j;
+      }
+    }
+    return tempArrayOfTodos.length+1 
+  }
+
   deleteTask(todo: task): void {
     console.log('удалил тудушку с тексом ', todo?.title);
     var index = this.tempArrayOfTodos.indexOf(todo);
     this.tempArrayOfTodos.splice(index, 1);     
+    this.serverRequestDelete(this.URLtoDelete, todo)
   }
 
   toggleStatus(todo: task): void {
     console.log(`переключил статус тудушки ${todo?.title} на ${!(todo?.status)}`);
     (todo as taskObj).status = !(todo as taskObj).status;
+    this.serverRequestPut(this.URLtoPut, todo)
   }
 
   createNewTodo(value: string): void {
-    console.log(`добавил тудушку с текстом ${value}`)
-    if (value) {
-      this.tempArrayOfTodos.push({id:1, title: value, status: true})
-      this.newTodotask = ''
+    if (value.split(' ').join('')) {
+      console.log(`добавил тудушку с текстом ${value}`)
+      let newTask = {id:this.foundFreeIndex(this.tempArrayOfTodos), title: value, status: true}
+      this.serverRequestPost(this.URLtoPost, newTask)
+      this.tempArrayOfTodos.push(newTask)
     }
+    this.newTodotask = ''
   }
 }
+
