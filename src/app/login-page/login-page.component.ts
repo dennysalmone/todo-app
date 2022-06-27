c6229785f80237057d319a7f047b2ab208ba4496
Denny <calgary.dany@gmail.com>
2022-06-27 21:37:12 +0300

    small fixes, add query

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -16,7 +16,7 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   aSub!: Subscription;
 
   constructor(private auth: AuthService, private router: Router, private route: ActivatedRoute) {
-
+ 
   }
 
   ngOnInit(): void {
